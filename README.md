5cf1d4614b4df1fda130f86b28a13d36764a7621
Anton Pham <anton@tekcent.com>
2022-07-13 00:19:29 +0700

    Use parms

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # home-speedtest
-Code for running speedtest on home internet connections
+Code for running speedtest on home internet connections.
+
+## Installation
+General install steps: https://github.com/influxdata/influxdb-client-python#installation
+
+1. `pip install 'influxdb-client[ciso]' speedtest-cli`
+
+
+
---
+++ speedtest-influx.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import sys
+from pydoc import cli
+import influxdb_client
+import speedtest
+
+from datetime import datetime
+
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+print(sys.argv[0])
+
+# You can generate an API token from the "API Tokens Tab" in the UI
+token = "KE760kQWgvqIJ1H0F9v6oeOUfB1O-gxX-HnqlLmZXdqGh0B77BMsAgSlcA8wwDukhnUcZlHAPHjS6NupNjqTCA=="
+org = "Home"
+bucket = "Internet"
+time = datetime.utcnow()
+
+# run a single-threaded speedtest using default server
+s = speedtest.Speedtest()
+s.get_best_server()
+s.download(threads=1)
+s.upload(threads=1)
+res = s.results.dict()
+
+with InfluxDBClient(url="http://10.88.88.10:49161", token=token, org=org) as client:
+    print(res)
+    body = [
+        {
+            "measurement": "speedtest",
+            "time": time,
+            "fields": {
+                "location": sys.argv[0],
+                "download": res["download"],
+                "upload": res["upload"],
+                "ping": res["ping"]
+            }
+        }
+    ]
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    query_api = client.query_api()
+    write_api.write(bucket=bucket, record=body)
+    client.close()
